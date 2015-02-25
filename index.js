a47d4b41489f77fd43bccac10f28815cb055132c
Arnold Almeida <arnold.almeida@gmail.com>
2015-02-25 18:52:50 +1100

    - Expose store prototype @jedwatson - Expose localstorage as module - crypto-browserify - Introduce ViewHistoryManager so we can implement backTo(index)

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var Touchstone = {
 	Navigation: require('./lib/mixins/Navigation'),
 	Dialogs: require('./lib/mixins/Dialogs'),
 	Link: require('./lib/components/Link'),
-	UI: require('./lib/ui')
+	UI: require('./lib/ui'),
+	Store: require('./lib/store'),
+	LocalStorage: require('./lib/storage')
 };
 
 module.exports = Touchstone;
